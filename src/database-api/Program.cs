48b691ca7ef750561b6f0d97d471b0e089fdf9b9
Petteri Mattila <petterimattila@protonmail.com>
2023-11-07 16:52:38 +0200

    Database directory renamed to lambdas

--- src/database-api/Program.cs
+++ src/database-api/Program.cs
@@ -14,16 +14,18 @@ builder.Services.AddSwaggerGen(c => {
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI(c => {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Database API V1");
-});
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Database API V1");
+    });
+}
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/sites", () => IndexDB.GetRecords());
 app.MapGet("/sites/{id}", (int id) => IndexDB.GetRecord(id));
-app.MapPost("/sites", (ScrapedSite site) => IndexDB.CreateRecord(site));
-app.MapPut("/sites", (ScrapedSite site) => IndexDB.UpdateRecord(site));
+app.MapPost("/sites", (Site site) => IndexDB.CreateRecord(site));
+app.MapPut("/sites", (Site site) => IndexDB.UpdateRecord(site));
 app.MapDelete("/sites/{id}", (int id) => IndexDB.RemoveRecord(id));
 
 app.Run();
--- src/database-api/lambdas/Db.cs
+++ src/database-api/lambdas/Db.cs
@@ -1,30 +1,31 @@
 namespace DatabaseApi.DB;
 
-public record ScrapedSite {
+public record Site {
     public int id { get; set; }
     public string? name { get; set; }
 }
 
 public class IndexDB {
-    private static List<ScrapedSite> _index = new List<ScrapedSite>() {
-        new ScrapedSite{ id=1, name="Testing 1" },
-        new ScrapedSite{ id=2, name="Testing 2" },
+    private static List<Site> _index = new List<Site>() 
+    {
+        new Site{ id=1, name="Testing 1" },
+        new Site{ id=2, name="Testing 2" },
     };
 
-    public static List<ScrapedSite> GetRecords() {
+    public static List<Site> GetRecords() {
         return _index;
     }
 
-    public static ScrapedSite? GetRecord(int id) {
+    public static Site? GetRecord(int id) {
         return _index.SingleOrDefault(site => site.id == id);
     }
 
-    public static ScrapedSite CreateRecord(ScrapedSite site) {
+    public static Site CreateRecord(Site site) {
         _index.Add(site);
         return site;
     }
 
-    public static ScrapedSite UpdateRecord(ScrapedSite update) {
+    public static Site UpdateRecord(Site update) {
         _index = _index.Select(site => {
             if (site.id == update.id) {
                 site.name = update.name;
