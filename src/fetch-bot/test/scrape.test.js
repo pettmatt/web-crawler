2f716569b1aa4952005e3f4a5441f439194e8a84
Petteri Mattila <petterimattila@protonmail.com>
2023-11-21 13:38:47 +0200

    Improving tests

--- src/fetch-bot/test/scrape.test.js
+++ src/fetch-bot/test/scrape.test.js
@@ -1,26 +1,52 @@
-import { scrapeHTML, validateHeadTags } from "../index"
+import { scrapeHTML, validateHeadTags } from "../lib/process"
 
-test("Validate HTML head tags", async () => {
-	try {
-        const html = `
-            <html>
-            <head>
-                <title>Test head<title>
-                <meta name="robots" content="noarchive" />
-            </head>
-            <body>
-                <h1>Test should fail if this is found</h1>
-            </body>
-            </html>
-        `
+describe("Validator can", () => {
+    test("validate HTML head tags", async () => {
+        try {
+            const html = `
+                <html>
+                <head>
+                    <title>Test head<title>
+                    <meta name="robots" content="noarchive" />
+                </head>
+                <body>
+                    <h1>Test should fail if this is found</h1>
+                </body>
+                </html>
+            `
+            const rules = ["noindex", "nofollow", "noarchive", "nosnippet"]
+            const validationResult = validateHeadTags(html, rules)
 
-        const rules = ["noindex", "nofollow", "noarchive", "nosnippet"]
+            expect(validationResult).toBeInstanceOf(Object)
+        } catch (error) {
+            throw new Error(`Head tag validator test failed with following excuse:\n${error}`)
+        }
+    })
 
-        const validationResult = validateHeadTags(html, rules)
-		console.log("Validator", validationResult)
+    test("scrape HTML", async () => {
+        try {
+            const html = `
+                <html>
+                <head>
+                    <meta robots="" />
+                </head>
+                <body>
+                    <h1>Test<h1>
+                    <p>Lorem ipsum</p>
+                    <a href="/internalLink">Internal</a>
+                    <a href="https://google.com">To somewhere</a>
+                </body>
+                </html>
+            `
+            const list = [{ link: "https://example.com", html }]
+            const scrapedResult = scrapeHTML(list)
 
-		expect(validationResult).toBeInstanceOf(Object)
-	} catch (error) {
-		throw new Error(`Head tag validator test failed with following excuse:\n${err}`)
-	}
+            expect(scrapedResult).toBeInstanceOf(Object)
+            expect(scrapedResult[0].page).toBeDefined()
+            expect(scrapedResult[0].scrapeResult).toBeDefined()
+            expect(scrapedResult[0].scrapeResult).toBeInstanceOf(Object)
+        } catch(error) {
+            throw new Error(`Scrape HTML test failed with following excuse:\n${error}`)
+        }
+    })
 })
