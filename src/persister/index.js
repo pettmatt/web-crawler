2794e8dbe713839579381e94f915fa1442b4fdd4
Petteri Mattila <petterimattila@protonmail.com>
2023-11-22 15:48:09 +0200

    Changing functionalities to expect one url at a time

--- src/persister/index.js
+++ src/persister/index.js
@@ -1,17 +1,11 @@
 import { parseTags, attributeFromTag } from "./lib/checker.js"
 
-function main() {
-    const html = `
-        <body>
-        <div>Do not want
-            <a href="">Want this 1</a>
-            <a href="https://google.com">Want this 2</a>
-            <p>paragraph</p>
-        </div>
-        </body>
-    `
+function processHTMLBody(page) {
+    const url = page[0].url
+    const head = page[0].details[0].scraped.head
+    const body = page[0].details[0].scraped.body
 
-    const linkTags = parseTags(html, "<a", "</a>")
+    const linkTags = parseTags(body, "<a", "</a>")
     const links = []
 
     for (let i = 0; i < linkTags.length; i++) {
@@ -19,11 +13,22 @@ function main() {
         links.push(link)
     }
 
+    links = links.filter((link) => link !== "")
+
     console.log("Tags", linkTags)
     console.log("Links", links)
+
+    return {
+        parentPage: url,
+        linksFound: links,
+        html: {
+            head,
+            body
+        }
+    }
 
     // Next these links should be returned to the validator, and the url of this page 
     // should be returned to the database with a guess what the page contains.
 }
 
-main()
+export default processHTMLBody
