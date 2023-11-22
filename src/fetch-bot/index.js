c208a30bc1ed5f05e7be0e53ca4d2355ba9a5a11
Petteri Mattila <petterimattila@protonmail.com>
2023-11-22 13:29:16 +0200

    Implementing amqp connection

--- src/fetch-bot/index.js
+++ src/fetch-bot/index.js
@@ -1,95 +1,29 @@
-function validateHeadTags(html, rule) {
-	const headStart = html.indexOf("<head>") + "<head>".length
-	const headEnd = html.indexOf("</head>")
-	const head = html.slice(headStart, headEnd)
+import { scrapeHTML } from "./lib/process.js"
 
-	const allInstancesOfRule = []
-
-	for (let i = 0; i < head.length; i++) {
-		if (head[i] === "<") {
-			const testInstanceOfRule = head.slice(i, i + rule.length)
-			if (testInstanceOfRule === rule) {
-				const endChars = "/>"
-				const metaTagEndIndex = head.indexOf(endChars, i) + endChars.length
-				const metaTag = head.slice(i, metaTagEndIndex)
-				allInstancesOfRule.push(metaTag)
-			}
-		}
-	}
-
-	return { robotsMetaTags: allInstancesOfRule, head }
-}
-
-function scrapeHTML(pages) {
-	const ruleTags = ["<meta name=\"robots\""]
-	const scrape = [["<body>", "</body>"]]
-	const followRules = ["noindex", "nofollow", "noarchive", "nosnippet"]
-
-	const validateResult = pages.map((page) => {
-		const { html, link } = page
-
-		const scrapeResult = ruleTags.map((rule) => {
-			const { robotsMetaTags, head } = validateHeadTags(html, rule)
-
-			const scrapedContent = scrape.map((chars) => {
-				const charsStart = html.indexOf(chars[0]) + chars[0].length
-				const charsEnd = html.indexOf(chars[1])
-				const content = html.slice(charsStart, charsEnd)
-				return content
-			})
-
-			if (robotsMetaTags.length > 0) {
-				const ruleContents = robotsMetaTags.map((ruleInstance) => {
-					const splitedMetaTag = ruleInstance.split("\"")
-					const robotRules = splitedMetaTag[3].split(", ")
-					const ruleList = robotRules.filter((r) => followRules.includes(r))
-					return ruleList
-				})
-
-				return {
-					rule,
-					metaTagsFound: true,
-					scraped: { head, scrapedContent },
-					metaTagRules: ruleContents,
-				}
-			}
-
-			return {
-				rule,
-				metaTagsFound: false,
-				scraped: { head, scrapedContent },
-			}
-		})
-
-		return { page: link, scrapeResult }
-	})
-
-	return validateResult
-}
-
-async function main() {
-	const links = [
-		"https://pettmatt.com",
-		"https://yle.fi",
-	]
-
+async function fetchAndProcessDataFromUrl(link) {
 	const htmlResponses = []
+	let index = 0
 
-	for (let link of links) {
-		// Add features that are common for bots.
-		// For example if fetch request fails -> fait 2 secs and try again
+	const intervalId = setInterval(fetchRequest, 2000)
+	await fetchRequest()
+
+	async function fetchRequest() {
 		const response = await fetch(link)
-		const html = await response.text()
-		htmlResponses.push({ link, html })
+
+		if (response.ok) {
+			const html = await response.text()
+			htmlResponses.push({ link, html })
+			clearInterval(intervalId)
+		} else if (index > 1 || response.status === 500) {
+			clearInterval(intervalId)
+		}
+
+		index++
 	}
 
 	const scrapedList = scrapeHTML(htmlResponses)
-	
-	for(let page of scrapedList) {
-		console.log(page.scrapeResult)
-	}
+
+	return scrapedList
 }
 
-main()
-
-export { scrapeHTML, validateHeadTags }
+export default fetchAndProcessDataFromUrl
