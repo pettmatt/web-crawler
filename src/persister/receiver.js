7042966f064a0352b0dd0bdb3d8b50f995094c0d
Petteri Mattila <petterimattila@protonmail.com>
2023-11-22 15:48:37 +0200

    including receiver and sender

---
+++ src/persister/receiver.js
@@ -0,0 +1,24 @@
+import amqp from "amqplib/callback_api.js"
+import processHTMLBody from "./index.js"
+import triggerSender from "./sender.js"
+
+amqp.connect("amqp://broker:test@172.20.0.4:5672", (error, connection) => {
+    if (error) throw error
+
+	connection.createChannel((error01, channel) => {
+        if (error01) throw error01
+
+        const queue = "persister_queue"
+        channel.assertQueue(queue, { durable: true })
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue)
+
+        channel.consume(queue, (message) => {
+            const pageDetails = JSON.parse(message.content.toString())
+            const processedPage = processHTMLBody(pageDetails)
+            console.log("Page", processedPage)
+
+            channel.ack(message)
+        }, { noAck: false })
+    })
+})
---
+++ src/persister/sender.js
@@ -0,0 +1,23 @@
+import amqp from "amqplib/callback_api.js"
+
+function triggerSender(url, queue = "validator_queue") {
+    amqp.connect(`amqp://broker:test@172.20.0.4:5672`, (error, connection) => {
+        if (error) throw error
+
+        connection.createChannel((error01, channel) => {
+            if (error01) throw error01
+
+            const message = JSON.stringify(url)
+
+            channel.assertQueue(queue, { durable: true })
+            channel.sendToQueue(queue, Buffer.from(message))
+
+            channel.close(() => {
+                connection.close()
+                process.exit(0)
+            })
+        })
+    })
+}
+
+export default triggerSender
