323b1e38efa4de6f999773a1498c6f96ba2c374f
pettmatt <petterimattila@protonmail.com>
2024-10-03 11:15:05 +0300

    Slight changes + basic styling

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-</script>
-
 <template>
-	
+	<TheHeader />
+	<TheSearch />
 </template>
 
 <style scoped>
 </style>
+
+<script setup lang="ts">
+import TheSearch from "./components/search/TheSearch.vue"
+import TheHeader from "./components/TheHeader.vue"
+</script>
