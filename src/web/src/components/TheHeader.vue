0f5e4766b9daf2b3b268c9086c50b1d22991d398
pettmatt <petterimattila@protonmail.com>
2024-10-03 11:14:24 +0300

    Vue components modified + moved

---
+++ src/web/src/components/TheHeader.vue
@@ -0,0 +1,10 @@
+<template>
+<h1>Web interface</h1>
+</template>
+
+<style scoped>
+</style>
+
+<script setup lang="ts">
+
+</script>
---
+++ src/web/src/components/search/TheSearch.vue
@@ -0,0 +1,12 @@
+<template>
+<input type="text" placeholder="Search">
+    
+</input>
+</template>
+
+<style scoped>
+</style>
+
+<script setup lang="ts">
+
+</script>
